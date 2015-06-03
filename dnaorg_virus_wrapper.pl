6699091cba75dcc24619a251cb6cfdc70e1bfed4
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2015-06-03 09:38:51 -0400

    Added -d option to specify output directory

--- dnaorg_virus_wrapper.pl
+++ dnaorg_virus_wrapper.pl
@@ -12,7 +12,15 @@ my $fetch_wrapper   = $exec_dir . "dnaorg_fetch_dna_wrapper.pl";
 my $parse_ftable    = $exec_dir . "dnaorg_parse_ftable.pl";
 my $compare_genomes = $exec_dir . "dnaorg_compare_genomes.pl";
 
-my $usage = "\ndnaorg_virus_wrapper.pl <list of accessions with representative genome first (must end in .ntlist)>\n";
+my $usage  = "\ndnaorg_virus_wrapper.pl\n";
+$usage .= "\t<list of accessions with representative genome first (must end in .ntlist)>\n";
+$usage .= "\n";
+$usage .= " OPTIONS:\n";
+$usage .= " -d <s> : create directory called <s> instead of auto-determined dir name\n";
+$usage .= "\n";
+
+my $outdir = undef;
+&GetOptions( "d=s" => \$outdir);
 
 if(scalar(@ARGV) != 1) { die $usage; }
 my ($listfile) = (@ARGV);
@@ -21,30 +29,35 @@ if($listfile !~ m/\.ntlist$/) { die "ERROR $listfile does not end in .ntlist"; }
 my $accn = $listfile;
 $accn =~ s/\.ntlist$//;
 
+if(! defined $outdir) { $outdir = $accn; }
+$outdir =~ s/\/$//; # remove final char if it's a '/'
+my $outdirroot = $outdir;
+$outdirroot =~ s/^.+\///;
+
 my $cmd;
 
 if(! -s $listfile) { die "ERROR $listfile does not exist or is empty"; }
 
 # Step 1: create feature table with dnaorg_fetch_dna_wrapper.pl:
 printf("Step 1: creating feature table ... ");
-$cmd = "perl $fetch_wrapper -f -ntlist -ftable -d $accn $listfile > /dev/null";
+$cmd = "perl $fetch_wrapper -f -ntlist -ftable -d $outdir $listfile > /dev/null";
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
 # Step 2: parse feature table into CDS and other tables with dnaorg_parse_ftable.pl:
 printf("Step 2: parsing feature table ... ");
-$cmd = "perl $parse_ftable -d $accn $accn/$accn.ftable $accn > /dev/null"; 
+$cmd = "perl $parse_ftable -d $outdirroot $outdir/$outdirroot.ftable $outdirroot > /dev/null"; 
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
 # Step 3: compare genomes 
 printf("Step 3: comparing genomes ... ");
-$cmd = "perl $compare_genomes $accn $accn/$accn.ntlist.not_suppressed > $accn/$accn.compare"; 
+$cmd = "perl $compare_genomes $outdir $outdir/$accn.ntlist.not_suppressed > $outdir/$outdirroot.compare"; 
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
 # Output $accn.compare file 
-$cmd = "cat $accn/$accn.compare";
+$cmd = "cat $outdir/$outdirroot.compare";
 runCommand($cmd, 0);
 
 #############
