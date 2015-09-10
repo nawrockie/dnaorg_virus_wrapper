f81e5b99a27703369cbbbc39994412abf08000cc
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2015-09-10 11:41:51 -0400

    Minor tweaks related to recent addition of -matpept

--- dnaorg_virus_wrapper.pl
+++ dnaorg_virus_wrapper.pl
@@ -17,11 +17,15 @@ my $usage  = "\ndnaorg_virus_wrapper.pl\n";
 $usage .= "\t<list of accessions with representative genome first (must end in .ntlist)>\n";
 $usage .= "\n";
 $usage .= " OPTIONS:\n";
-$usage .= " -d <s> : create directory called <s> instead of auto-determined dir name\n";
+$usage .= " -d <s>   : create directory called <s> instead of auto-determined dir name\n";
+$usage .= " -matpept : run dnaorg_fetch_dna_wrapper.pl with -matpept instead of -ftable\n";
 $usage .= "\n";
 
 my $outdir = undef;
-&GetOptions( "d=s" => \$outdir);
+my $do_matpept = 0;
+&GetOptions( 
+    "d=s" => \$outdir,
+    "matpept" => \$do_matpept);
 
 if(scalar(@ARGV) != 1) { die $usage; }
 my ($listfile) = (@ARGV);
@@ -36,25 +40,31 @@ my $outdirroot = $outdir;
 $outdirroot =~ s/^.+\///;
 
 my $cmd;
+my $option;
+my $infile;
 
 if(! -s $listfile) { die "ERROR $listfile does not exist or is empty"; }
-
-# Step 1: create feature table with dnaorg_fetch_dna_wrapper.pl:
-printf("Step 1: creating feature table ... ");
-$cmd = "perl $fetch_wrapper -f -ntlist -ftable -d $outdir $listfile > /dev/null";
+# Step 1: create feature table or fetch mat_peptide info with dnaorg_fetch_dna_wrapper.pl:
+printf("%s ... ", ($do_matpept) ? "Step 1: fetching mat_peptide info" : "Step 1: creating feature table");
+$option = ($do_matpept) ? "-matpept" : "ftable";
+$cmd    = "perl $fetch_wrapper -f -ntlist $option -d $outdir $listfile > /dev/null";
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
-# Step 2: parse feature table into CDS and other tables with dnaorg_parse_ftable.pl:
+# Step 2: parse feature table into CDS and other tables (or mat_peptide) table with dnaorg_parse_ftable.pl OR
+#         parse mat_peptide data into a MATPEPT table
 printf("Step 2: parsing feature table ... ");
-$cmd = "perl $parse_ftable -d $outdirroot $outdir/$outdirroot.ftable $outdirroot > /dev/null"; 
+$option = ($do_matpept) ? "-matpept" : "";
+$infile = ($do_matpept) ? "$outdir/$outdirroot.mat_peptide" : "$outdir/$outdirroot.ftable";
+$cmd    = "perl $parse_ftable $option -d $outdirroot $infile $outdirroot > /dev/null"; 
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
 # Step 3: compare genomes 
 printf("Step 3: comparing genomes ... ");
-my$compare_outfile = "$outdir/$outdirroot.compare"; 
-$cmd = "perl $compare_genomes -s -product -protid -codonstart $outdir $outdir/$accn.ntlist.not_suppressed > $compare_outfile"; 
+my $compare_outfile = "$outdir/$outdirroot.compare"; 
+$option = ($do_matpept) ? "-matpept" : "-protid -codonstart";
+$cmd = "perl $compare_genomes $option -s -product $outdir $outdir/$accn.ntlist.not_suppressed > $compare_outfile"; 
 runCommand($cmd, 0);
 printf("done. [$cmd]\n");
 
